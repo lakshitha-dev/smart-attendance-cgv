9a8aadcd19d73113890a696e7ce2c3bc4d42e0d7
mlswijerathne <mlswijerathne@gmail.com>
2026-07-16 00:28:59 +0530

    Revert "Merge pull request #5 from lakshitha-dev/fix-localization-detection"
    This reverts commit c7cfae7745a72f134598a3f803164d08cf8903c6, reversing
    changes made to 5b5e95256cf8453e4b9e829a3749a0251997ed2a.

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Suppress OpenCV GUI windows for the whole suite: CLI tests drive sams.main
-# end-to-end, and stage windows must neither strobe on desktops nor crash
-# headless CI runners (cli_display.py honors this seam).
-os.environ.setdefault("SAMS_HEADLESS", "1")
